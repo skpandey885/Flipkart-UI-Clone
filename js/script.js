7d72a6266b47bb1f3720621c20f68540d2dfb337
Sachin Pandey <skpandey885@gmail.com>
2021-09-12 15:10:13 +0530

    Js Folder Improved

---
+++ js/script.js
@@ -0,0 +1,70 @@
+
+function func1() {
+
+  let x = document.getElementById('arrow');
+  x.style.transform = "rotate(450deg)";
+
+}
+function func2() {
+  let x = document.getElementById('arrow');
+  x.style.transform = "rotate(270deg)";
+}
+
+function func3() {
+
+  let x = document.getElementsByClassName('_2sVfI7')[0];
+  x.style.transform = "rotate(450deg)";
+
+}
+function func4() {
+  let x = document.getElementsByClassName('_2sVfI7')[0];
+  x.style.transform = "rotate(270deg)";
+}
+
+function func5() {
+
+let x = document.getElementsByClassName('_2sVfI7')[1];
+x.style.transform = "rotate(450deg)";
+
+}
+function func6() {
+let x = document.getElementsByClassName('_2sVfI7')[1];
+x.style.transform = "rotate(270deg)";
+}
+
+function func7() {
+
+let x = document.getElementsByClassName('_2sVfI7')[2];
+x.style.transform = "rotate(450deg)";
+
+}
+function func8() {
+let x = document.getElementsByClassName('_2sVfI7')[2];
+x.style.transform = "rotate(270deg)";
+}
+
+function func9() {
+
+let x = document.getElementsByClassName('_2sVfI7')[3];
+x.style.transform = "rotate(450deg)";
+
+}
+function func10() {
+let x = document.getElementsByClassName('_2sVfI7')[3];
+x.style.transform = "rotate(270deg)";
+}
+
+
+function func11() {
+
+let x = document.getElementsByClassName('_2sVfI7')[4];
+x.style.transform = "rotate(450deg)";
+
+}
+function func12() {
+let x = document.getElementsByClassName('_2sVfI7')[4];
+x.style.transform = "rotate(270deg)";
+}
+
+
+
